919d241bcc2855bc472ae4922d7ab0470b957c77
ShimJiHoon <wlgns2223@gmail.com>
2022-10-31 00:13:43 +0900

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Main } from "./components/main";
+import { Background } from "./core/ui/background";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Background>
+                <div className="w-96 relative top-20 ">
+                    <Main />
+                </div>
+            </Background>
+        </div>
+    );
 }
 
 export default App;
